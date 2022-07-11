64371fe0771dcc7dc6efd43a286eb58e738734c7
Bhatti <usama.skipper0x@gmail.com>
2022-07-11 16:37:18 +0500

    - movement props serialize

--- Source/Tanky/Public/Pawns/PawnTank.h
+++ Source/Tanky/Public/Pawns/PawnTank.h
@@ -15,13 +15,16 @@ class TANKY_API APawnTank : public APawnBase
 	GENERATED_BODY()
 
 private:
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components", meta=(AllowPrivateAccess = true))
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = true))
 	USpringArmComponent* SpringArm;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components", meta=(AllowPrivateAccess = true))
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = true))
 	UCameraComponent* Camera;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement", meta = (AllowPrivateAccess = true))
 	float MoveSpeed = 100.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement", meta = (AllowPrivateAccess = true))
 	float RotateSpeed = 100.0f;
 
 	FVector MoveDirection;
